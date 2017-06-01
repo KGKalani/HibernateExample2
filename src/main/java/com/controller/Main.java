eb4275e11ae3e2897f81a65e41a5c23f43046faf
gayathri <gayathri.kalani@ge.com>
2017-06-01 17:16:27 +0530

    Create_Service_to_add_new_employee

--- src/main/java/com/controller/Main.java
+++ src/main/java/com/controller/Main.java
@@ -1,8 +1,29 @@
 package main.java.com.controller;
 
+import main.java.com.model.Employee;
+import main.java.com.service.EmployeeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by gayathri on 6/1/17.
  */
 public class Main {
+    public static void main(String[] args){
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("Jhon");
+        employee.setSalary(20000.00);
 
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        EmployeeService employeeService = (EmployeeService)context.getBean("employeeService");
+        employeeService.saveEmployee(employee);
+    }
 }
+
+/*    String configFileName = "none";
+            configFileName = System.getProperty(ControlWebGlobalConstants.log_configuration, configFileName);
+                    org.apache.log4j.PropertyConfigurator.configure(configFileName);
+                    ApplicationContext springContext = new ClassPathXmlApplicationContext(ControlWebGlobalConstants.paths);
+                    CHDXProcessService chdxProcessService = (CHDXProcessService) springContext.getBean("chdxProcessService");
+                    chdxProcessService.saveFile();*/
--- src/main/java/com/service/impl/EmpliyeeServiceImp.java
+++ src/main/java/com/service/impl/EmpliyeeServiceImp.java
@@ -1,5 +1,6 @@
 package main.java.com.service.impl;
 
+import main.java.com.dao.EmployeeDao;
 import main.java.com.model.Employee;
 import main.java.com.service.EmployeeService;
 
@@ -7,8 +8,16 @@ import main.java.com.service.EmployeeService;
  * Created by gayathri on 6/1/17.
  */
 public class EmpliyeeServiceImp implements EmployeeService {
-    @Override
-    public void saveEmployee(Employee employee) {
 
+    private EmployeeDao employeeDao;
+
+    public void setEmployeeDao(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+    @Override
+
+    public void saveEmployee(Employee employee) {
+        employeeDao.saveEmployee(employee);
     }
 }
